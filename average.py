da1d46e40257278d0061261fcbd0ddca0cc7a884
sophiacphilips <114261752+sophiacphilips@users.noreply.github.com>
2022-10-05 18:55:40 -0700

    Wrote program that will ask user for 5 numbers and give them the average.

---
+++ average.py
@@ -0,0 +1,13 @@
+#Author: Sophia Philips
+#GitHub Username: sophiacphilips
+#Date: 10/05/23
+#Description: This program asks for five numbers and prints the average of them.
+
+print("Please enter five numbers.")
+num_1=float(input())
+num_2=float(input())
+num_3=float(input())
+num_4=float(input())
+num_5=float(input())
+print("The average of those numbers is:")
+print((num_1+num_2+num_3+num_4+num_5)/5)
